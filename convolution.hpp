5097096f44431aecab2c501636a1b199fb6f3dc1
John Gliksberg <jg.trosh@gmail.com>
2015-11-22 17:58:41 +0100

    retab, add missing }, fix ndeb*3 offset

--- convolution.hpp
+++ convolution.hpp
@@ -27,64 +27,59 @@ public:
             int rank, int ndeb, int nfin,
             int size_bloc, int reste, bool p3)
     {
-
-
-	if (p3){
-		    for (int i=ndeb; i<nfin;           i++)
-		    for (int j=0;    j<image.gwidth(); j++)
-		    {
-		        T red = 0;
-		        T green = 0;
-		        T blue = 0;
-
-		        for (int k=0; k<filtre.height; k++)
-		        for (int l=0; l<filtre.width;  l++)
-		        {
-		            int x = j - filtre.width/2 + l;
-		            x += image.gwidth();
-		            x %= image.gwidth();
-		            int y = i - filtre.height/2 + k;
-		            y += image.gheight();
-		            y %= image.gheight();
-		            T val = filtre.filtre_val[k * filtre.width + l];
-		                red   += image.gval(x*3 + 0, y*3) * val;
-		                green += image.gval(x*3 + 1, y*3) * val;
-		                blue  += image.gval(x*3 + 2, y*3) * val;
-		        }
-		        
-		            part[i*image.gwidth()*3 + j*3 - ndeb*image.gwidth()]
-		            = int(/*filtre.factor*/MIN(image.level, MAX(0, red)));
-		            part[i*image.gwidth()*3 + j*3 + 1 - ndeb*image.gwidth()]
-		            = int(/*filtre.factor*/MIN(image.level, MAX(0, green)));
-		            part[i*image.gwidth()*3 + j*3 + 2- ndeb*image.gwidth()]
-		            = int(/*filtre.factor*/MIN(image.level, MAX(0, blue)));
-		    }
-	}
-	else{
-		for (int i=ndeb; i<nfin;           i++)
-		for (int j=0;    j<image.gwidth(); j++)
-		{
-		        T grey = 0;
-
-		        for (int k=0; k<filtre.height; k++)
-		        for (int l=0; l<filtre.width;  l++)
-		        {
-		            int x = j - filtre.width/2 + l;
-		            x += image.gwidth();
-		            x %= image.gwidth();
-		            int y = i - filtre.height/2 + k;
-		            y += image.gheight();
-		            y %= image.gheight();
-		            T val = filtre.filtre_val[k * filtre.width + l];
-					grey   += image.gval(x, y) * val;
-		        }
-		     
-				part[i*image.gwidth() + j - ndeb*image.gwidth()]
-		        = int(MIN(image.level, MAX(0, grey)));   
-
-		}
-
-	}
+        if (p3) /* COLOR */
+        {
+            for (int i=ndeb; i<nfin;           i++)
+            for (int j=0;    j<image.gwidth(); j++)
+            {
+                T red = 0;
+                T green = 0;
+                T blue = 0;
+                for (int k=0; k<filtre.height; k++)
+                for (int l=0; l<filtre.width;  l++)
+                {
+                    int x = j - filtre.width/2 + l;
+                    x += image.gwidth();
+                    x %= image.gwidth();
+                    int y = i - filtre.height/2 + k;
+                    y += image.gheight();
+                    y %= image.gheight();
+                    T filtre_value = filtre.filtre_val[k * filtre.width + l];
+                    /* * filtre.factor ? */
+                    red   += image.gval(x*3+0, y*3) * filtre_value;
+                    green += image.gval(x*3+1, y*3) * filtre_value;
+                    blue  += image.gval(x*3+2, y*3) * filtre_value;
+                }
+                int partpos = ( (i-ndeb)*image.gwidth() + j ) * 3;
+                part[partpos + 0] = static_cast<int>(MIN(image.level, MAX(0, red)));
+                part[partpos + 1] = static_cast<int>(MIN(image.level, MAX(0, green)));
+                part[partpos + 2] = static_cast<int>(MIN(image.level, MAX(0, blue)));
+            }
+        }
+        else
+        {
+            for (int i=ndeb; i<nfin;           i++)
+            for (int j=0;    j<image.gwidth(); j++)
+            {
+                T grey = 0;
+                for (int k=0; k<filtre.height; k++)
+                for (int l=0; l<filtre.width;  l++)
+                {
+                    int x = j - filtre.width/2 + l;
+                    x += image.gwidth();
+                    x %= image.gwidth();
+                    int y = i - filtre.height/2 + k;
+                    y += image.gheight();
+                    y %= image.gheight();
+                    T filtre_value = filtre.filtre_val[k * filtre.width + l];
+                    /* * filtre.factor ? */
+                    grey += image.gval(x, y) * filtre_value;
+                }
+                int partpos = (i-ndeb)*image.gwidth() + j;
+                part[partpos] = static_cast<int>(MIN(image.level, MAX(0, grey)));
+            }
+        }
+    }
     //void operator()(const Filter& filter);
 private:
     Image<T> image;
